0816c9c399d9b7e401e9010fcc28c6deaacc6409
Souradeep <souradeepbera162@gmail.com>
2023-01-08 20:34:10 +0530

    Added Temparature & Humidity....

--- index.js
+++ index.js
@@ -1,110 +1,108 @@
-// *Eta Diye Value Publish Hocche to MQTT Broker*
-
 const express = require('express');
 const path = require('path');
-const mqtt = require("mqtt");
-// var client = mqtt.connect('mqtt://test.mosquitto.org');
 const app = express()
 const http = require('http');
+const mqtt = require("mqtt");
 const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
-port = process.env.PORT || 3000;
+const port = process.env.PORT || 3000;
 
-let value;
-// ClientId for mqttClient and host
 const clientId = 'mqttjs_' + Math.random().toString(16).substr(2, 8)
-// MattHost
 const host = 'tcp://91.121.93.94'
 
+let SOCKET_CLIENT;
+
+
 app.use(express.json());
+
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
-
 app.use('/folder', express.static(path.join(__dirname, 'public2')))
 app.use('/about', express.static(path.join(__dirname, 'about')))
 app.use('/more', express.static(path.join(__dirname, 'more')))
 app.use('/contacts', express.static(path.join(__dirname, 'contacts')))
 
 app.post('/button', (req, res) => {
-    value = req.body
-    console.log(req.body)
-     //html css js
+  value = req.body
+  console.log(req.body)
+   //html css js
 if(req.body.button_one_value === true){
-    res.send({
-        "status": true,
-        "messege": "mqtt button1 is clicked"
-      })
-    let sendMessage = "P";
-    client.publish('sonu', sendMessage, { qos: 0, retain: false })
+  res.send({
+      "status": true,
+      "messege": "mqtt button1 is clicked"
+    })
+  let sendMessage = "P";
+  client.publish('sonu', sendMessage, { qos: 0, retain: false })
+  console.log("Button1");
 }
 else if(req.body.button_one_value === false){
-    res.send({
-        "status": true,
-        "messege": "mqtt button2 is clicked"
-      })
-    let sendMessage = "Q";
-    client.publish('sonu', sendMessage, { qos: 0, retain: false }) 
+  res.send({
+      "status": true,
+      "messege": "mqtt button2 is clicked"
+    })
+  let sendMessage = "Q";
+  client.publish('sonu', sendMessage, { qos: 0, retain: false }) 
 }
 
 else if(req.body.button_two_value === true){
-  res.send({
-      "status": true,
-      "messege": "mqtt button2 is clicked"
-    })
-  let sendMessage = "R";
-  client.publish('sonu', sendMessage, { qos: 0, retain: false }) 
+res.send({
+    "status": true,
+    "messege": "mqtt button2 is clicked"
+  })
+let sendMessage = "R";
+client.publish('sonu', sendMessage, { qos: 0, retain: false }) 
 }
 
 else if(req.body.button_two_value === false){
-  res.send({
-      "status": true,
-      "messege": "mqtt button2 is clicked"
-    })
-  let sendMessage = "S";
-  client.publish('sonu', sendMessage, { qos: 0, retain: false }) 
+res.send({
+    "status": true,
+    "messege": "mqtt button2 is clicked"
+  })
+let sendMessage = "S";
+client.publish('sonu', sendMessage, { qos: 0, retain: false }) 
 }
 
 else if(req.body.button_three_value === true){
-  res.send({
-      "status": true,
-      "messege": "mqtt button2 is clicked"
-    })
-  let sendMessage = "T";
-  client.publish('sonu', sendMessage, { qos: 0, retain: false }) 
+res.send({
+    "status": true,
+    "messege": "mqtt button2 is clicked"
+  })
+let sendMessage = "T";
+client.publish('sonu', sendMessage, { qos: 0, retain: false }) 
 }
 
 else if(req.body.button_three_value === false){
-  res.send({
-      "status": true,
-      "messege": "mqtt button2 is clicked"
-    })
-  let sendMessage = "U";
-  client.publish('sonu', sendMessage, { qos: 0, retain: false }) 
+res.send({
+    "status": true,
+    "messege": "mqtt button2 is clicked"
+  })
+let sendMessage = "U";
+client.publish('sonu', sendMessage, { qos: 0, retain: false }) 
 }
 
 else if(req.body.button_four_value === true){
-  res.send({
-      "status": true,
-      "messege": "mqtt button2 is clicked"
-    })
-  let sendMessage = "V";
-  client.publish('sonu', sendMessage, { qos: 0, retain: false }) 
+res.send({
+    "status": true,
+    "messege": "mqtt button2 is clicked"
+  })
+let sendMessage = "V";
+client.publish('sonu', sendMessage, { qos: 0, retain: false }) 
 }
 
 
 else if(req.body.button_four_value === false){
-  res.send({
-      "status": true,
-      "messege": "mqtt button2 is clicked"
-    })
-  let sendMessage = "W";
-  client.publish('sonu', sendMessage, { qos: 0, retain: false }) 
+res.send({
+    "status": true,
+    "messege": "mqtt button2 is clicked"
+  })
+let sendMessage = "W";
+client.publish('sonu', sendMessage, { qos: 0, retain: false }) 
 }
 
 else{
-  console.log("O");
+console.log("O");
 }
 
 
@@ -112,98 +110,95 @@ else{
 
 
 
-    
-    
+  
+  
 })
 
-
-
-app.listen(port, () => {
+server.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
 })
 
 
-
-
-
-
-// const host = 'ws://127.0.0.1:1883'
-
-// let sendMessage=process.argv[2]
-
-
 const options = {
-  keepalive: 30,
-  clientId: clientId,
-  protocolId: 'MQTT',
-  protocolVersion: 4,
-  clean: true,
-  reconnectPeriod: 1000,
-  connectTimeout: 30 * 1000,
-  will: {
-    topic: 'WillMsg',
-    payload: 'Connection Closed abnormally..!',
-    qos: 0,
-    retain: false
-  },
-  rejectUnauthorized: false
-}
+    keepalive: 30,
+    clientId: clientId,
+    protocolId: 'MQTT',
+    protocolVersion: 4,
+    clean: true,
+    reconnectPeriod: 1000,
+    connectTimeout: 30 * 1000,
+    will: {
+      topic: 'WillMsg',
+      payload: 'Connection Closed abnormally..!',
+      qos: 0,
+      retain: false
+    },
+    rejectUnauthorized: false
+  }
+  
+  console.log('connecting mqtt client')
+  const client = mqtt.connect(host, options)
 
-console.log('connecting mqtt client')
-const client = mqtt.connect(host, options)
 
-client.on('error', function (err) {
+  client.on('error', function (err) {
   console.log(err)
   client.end()
 })
 
-client.on('connect', function () {
-  console.log('client connected:' + clientId)
-  // console.log("");
-  client.subscribe('sonu', { qos: 0 },(sub_message)=>{
-      console.log("Message From Server : "+sub_message);
-  })
-//   client.subscribe('topic', { qos: 1 })
-
-//   client.publish('topic', sendMessage, { qos: 0, retain: false })
-})
-
-client.on('message', function (topic, message, packet) {
-  console.log('Received Message:= ' + message.toString() + '\nOn topic:= ' + topic)
-  // console.log("\n"+JSON.stringify(packet))
-})
 
 client.on('connect', function () {
-  console.log('client connected:' + clientId)
-  // console.log("");
-  client.subscribe('weather', { qos: 0 },(sub_message)=>{
-      console.log("Message From Server : "+sub_message);
-      client.on('message', function (topic, message, packet) {
-        console.log('Received Message:= ' + message.toString() + '\nOn topic:= ' + topic)
-        msg = message.toString();
-        io.on('connection', (socket) => {
-          
-          io.broadcast.emit('Weather', msg);
-          
-          
-        });
-        // console.log("\n"+JSON.stringify(packet))
-      })
+      console.log('client connected:' + clientId)
+      client.subscribe('esp32_mqtt_tempvalue', { qos: 0 },(sub_message)=>{
+          console.log("Message From Server : "+sub_message); 
+        }) 
+        
+      client.subscribe('esp32_mqtt_temp_humidvalue', { qos: 0 },(sub_message)=>{
+            console.log("Message From Server : "+sub_message); 
+        })  
     })
+
+
 
     
 
-})
 
 
-
-
-
-
-
-
-
-//client on
-client.on('close', function () {
-  console.log(clientId + ' disconnected')
-})
+try {
+  
+    io.on("connection", (socket) => {
+      SOCKET_CLIENT =  socket;
+      console.log(`User with id: ${socket.id} connected!`);
+  
+      socket.on("disconnect", () => {
+        console.log(`User with id: ${socket.id} disconnected`);
+      });
+    
+  
+      // io.emit("Chart-Data",message)
+      socket.on("hello_server", (data) => {
+        console.log(data);
+        let senddata = "temp";
+        
+        socket.emit("weather", senddata);
+      });
+      client.on('message', function (topic, message, packet) {
+        // console.log('Received Message:= ' + message.toString() + '\nOn topic:= ' + topic)
+      //   console.log("\n"+JSON.stringify(packet))
+       if(topic === "esp32_mqtt_tempvalue"){
+          SOCKET_CLIENT.emit("weather", message.toString());
+       }
+       else if(topic === "esp32_mqtt_temp_humidvalue"){
+          
+          SOCKET_CLIENT.emit("hum", message.toString());
+       }
+        
+      })
+   
+  
+    });
+  
+    //admin-ui
+    // instrument(io, { auth: false });
+  } catch (error) {
+    console.log(`Could not start the server, ${error}`);
+  }
